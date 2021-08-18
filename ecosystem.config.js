f77372a1ba9a52bad45f28a9d79bbb0942a6dd05
Tan Zon <tanhoang.cntt@gmail.com>
2021-08-18 17:40:06 +0700

    pm2 win

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [{
+        name: 'game-autoplay',
+        script: 'dist/main.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        time: true,
+        env: {
+        },
+    }],
+}
